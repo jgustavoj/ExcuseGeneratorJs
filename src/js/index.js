3573aff1f87bbec558a976874bd66bed712fe47b
jgustavoj <gjime011@fiu.edu>
2020-08-12 22:06:36 +0000

    first commit

--- src/js/index.js
+++ src/js/index.js
@@ -8,3 +8,40 @@ import "../style/index.scss";
 window.onload = function() {
   console.log("Hello Rigo from the console!");
 };
+
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+let randomWho = Math.floor(Math.random() * who.length);
+
+console.log(randomWho);
+
+let randomAction = Math.floor(Math.random() * action.length);
+
+console.log(randomAction);
+
+let randomWhat = Math.floor(Math.random() * what.length);
+
+console.log(randomWhat);
+
+let randomWhen = Math.floor(Math.random() * when.length);
+
+console.log(randomWhen);
+
+let newSentence =
+  who[randomWho] +
+  " " +
+  action[randomAction] +
+  " " +
+  what[randomWhat] +
+  " " +
+  when[randomWhen];
+
+document.querySelector("#excuse").innerHTML = newSentence;
